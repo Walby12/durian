9b9c2ff212353ed73125822a8e67d780ca908501
Walby12 <alby09.bonf@gmail.com>
2025-09-08 18:33:04 +0200

    Added int tokens

--- src/main.c
+++ src/main.c
@@ -8,13 +8,16 @@ enum Tokens {
   ADD,
   SUB,
   PUSH,
-  POP
+  POP,
+  INT
 };
 
 struct Durian {
   int index;
   int line;
   int pc;
+  int values;
+  int ints[1024];
 };
 
 enum Tokens* tokenize(const char *src, int *count, struct Durian *dur) {
@@ -38,7 +41,7 @@ enum Tokens* tokenize(const char *src, int *count, struct Durian *dur) {
     } else if (isalpha((unsigned char)c)) {
       char str[1024];
       int j = 0;
-      while (isalpha((unsigned char)src[dur->pc]) || j == len) {
+      while (isalpha((unsigned char)src[dur->pc]) && j < len) {
         str[j++] = src[dur->pc++];
       }
       str[j] = '\0';
@@ -58,6 +61,17 @@ enum Tokens* tokenize(const char *src, int *count, struct Durian *dur) {
       dur->line++;
       dur->pc++;
       dur->index = 0;
+    } else if (isdigit((unsigned char)c)) {
+      char str[1024];
+      int j = 0;
+      while (isdigit((unsigned char)src[dur->pc]) && j < len) {
+        str[j++] = src[dur->pc++];
+      }
+      str[j] = '\0';
+
+      toks[t++] = INT;
+      dur->ints[dur->values++] = atoi(str);
+      dur->index++;
     } else {
       if (!isspace(c)) {
         printf("Unrecognized char: %c (token number: %d ,line: %d)\n", c, dur->index+1, dur->line+1);
@@ -73,38 +87,60 @@ enum Tokens* tokenize(const char *src, int *count, struct Durian *dur) {
 }
 
 int main(int argc, char *argv[]) {
-  char src[1024];
-
   bool cond = false;
-
+  char *src = NULL;
   if (argc < 2) {
     printf("Error you did not pass enough arguments\n");
   } else if (argc == 2) {
-    FILE *f = fopen(argv[1], "r");
+    FILE *f = fopen(argv[1], "rb");
     cond = true;
     if (f == NULL) {
       printf("Error could not open file\n"); 
       return 0;
     } else {
-      fgets(src, 1024, f);
+      fseek(f, 0, SEEK_END);
+      long size = ftell(f);
+      rewind(f);
+
+      src = malloc(size + 1);
+      if (src == NULL) {
+        printf("ERROR: ran out of memory\n");
+        fclose(f);
+        return 1;
+      }
+
+      size_t bytes_read = fread(src, 1, size, f);
+      src[bytes_read] = '\0';
+
       fclose(f);
     }
   } else {
-    printf("You passed to many args\n");
+    printf("You passed too many args\n");
   }
 
-  if (cond) {
-    struct Durian dur = {0, 0, 0};
+  if (cond && src != NULL) {
+    struct Durian dur = {0};
   
     int count;
     enum Tokens *toks = tokenize(src, &count, &dur);
-    if (toks == NULL) return 0; 
+    if (toks == NULL) {
+      free(src);
+      return 0; 
+    }
+
     for (int i = 0; i < count; i++) {
       printf("%d ", toks[i]);
     }
     printf("\n");
 
+    for (int j = 0; j < dur.values; j++) {
+      printf("%d ", dur.ints[j]);
+    }
+    printf("\n");
+
     free(toks);
+    free(src);
   }
+
   return 0;
 }
