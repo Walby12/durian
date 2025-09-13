9bfc3695a279d460fbd8e4586716d4a472621f0c
Walby12 <alby09.bonf@gmail.com>
2025-09-13 20:08:41 +0200

    Started working on the ast builder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::process::*;
+use std::env::*;
+use std::fs::*;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Tokens {
     Add,
     Sub,
@@ -9,7 +11,20 @@ enum Tokens {
     Int(i64),
 }
 
-fn tokenize(src: String) -> Vec<Tokens> {
+#[derive(Debug)]
+enum Errors {
+    PeekLeftOfb,
+    PeekRight,
+    OutOfBounds
+}
+
+#[derive(Debug)]
+struct Durian {
+    token: i64,
+    line: i64,
+}
+
+fn tokenize(src: String, dur: &mut Durian) -> Vec<Tokens> {
     let chars: Vec<char> = src.chars().collect();
     let mut tokens: Vec<Tokens> = Vec::new();
     let mut i = 0;
@@ -18,9 +33,9 @@ fn tokenize(src: String) -> Vec<Tokens> {
         let c = chars[i];
 
         match c {
-            '+' => { tokens.push(Tokens::Add); i += 1; }
-            '-' => { tokens.push(Tokens::Sub); i += 1; }
-            mut c if c.is_alphabetic() => {
+            '+' => { tokens.push(Tokens::Add); i += 1; dur.token += 1; }
+            '-' => { tokens.push(Tokens::Sub); i += 1; dur.token += 1; }
+            c if c.is_alphabetic() => {
                 let mut string: String = String::new();
                 
                 while i < chars.len() && chars[i].is_alphabetic(){
@@ -29,31 +44,34 @@ fn tokenize(src: String) -> Vec<Tokens> {
                 }
                 
                 match string.as_str() {
-                    "push" => { tokens.push(Tokens::Push); }
-                    "pop" => { tokens.push(Tokens::Pop); }
-                    _ => { eprintln!("Unrecognized string: {}", string); exit(1); }
+                    "push" => { tokens.push(Tokens::Push); dur.token += 1; }
+                    "pop" => { tokens.push(Tokens::Pop); dur.token += 1; }
+                    _ => { eprintln!("Unrecognized string: {} at token: {:?} at line: {:?}", string, dur.token, dur.line); exit(1); }
                 }
             }
-            mut c if c.is_digit(10) => {
+            c if c.is_digit(10) => {
                 let mut string: String = String::new();
 
                 while i < chars.len() && chars[i].is_digit(10) {
-                    string.push_str(&c.to_string());
+                    string.push_str(&chars[i].to_string());
                     i += 1;
                 }
 
                 let num = string.parse::<i64>();
 
                 match num {
-                    Ok(n) => { tokens.push(Tokens::Int(n)); }
+                    Ok(n) => { tokens.push(Tokens::Int(n)); dur.token += 1; }
                     Err(e) => { eprintln!("Could not parse int: {}, error: {}", string, e); exit(1); }
                 }
             }
             c if c.is_whitespace() => {
+                if c == '\n' {
+                    dur.line += 1;
+                }
                 i += 1;
             }
             _ => {
-                eprintln!("Unrecognized char: {}", c);
+                eprintln!("Unrecognized char: {} at token {:?} at line {:?}", c, dur.token, dur.line);
                 exit(1);
             }
         }
@@ -61,7 +79,78 @@ fn tokenize(src: String) -> Vec<Tokens> {
     tokens
 }
 
+fn peek_l(tokens: &Vec<Tokens>, index: &usize) -> Result<Tokens, Errors> {
+    if *index == 0 {
+        return Err(Errors::PeekLeftOfb);
+    }
+
+    let i = (*index - 1) as usize;
+
+    Ok(tokens[i])
+}
+
+fn peek_r(tokens: &Vec<Tokens>, index: &usize) -> Result<Tokens, Errors> {
+    if *index == 0 {
+        return Err(Errors::PeekLeftOfb);
+    }
+
+    let i = (*index + 1) as usize;
+
+    Ok(tokens[i])
+}
+
+fn build_program(tokens: &Vec<Tokens>, dur: &mut Durian) {
+    let mut string_builder: String = String::new();
+    let mut index: usize = 0;
+    let mut tok = 0;
+    let mut line = 1;
+
+    while index < tokens.len() {
+        let c = tokens[index];
+
+        match c {
+            Tokens::Add => {
+                let l = match peek_l(&tokens, &index) {
+                    Ok(t) => t,
+                    Err(_) => { 
+                        eprintln!("Tried to peek the token previous to the plus op at token {} at line {} but went out of bounds", tok, line); 
+                        exit(1); 
+                    }
+                };
+                
+                if matches!(l, Tokens::Int(_)) || matches!(l, Tokens::Pop) {
+                    eprintln!("Error got token: {:?}, but expected an int or a pop op.\nError occurred at token {} at line {}", l, tok, line);
+                    exit(1);
+                }
+                
+            }
+        }
+        index += 1;
+    }
+}
+
 fn main() {
-    let toks = tokenize(String::from("+ - push pop 12"));
+    let mut dur = Durian {
+        token: 1,
+        line: 1,
+    };
+    
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        eprintln!("You did not pass enough args");
+        exit(1);
+    }
+
+    let file_name = args[1].clone();
+
+    let file = read_to_string(file_name);
+
+    let src = match file {
+        Ok(src) => { src }
+        Err(e) => { eprintln!("Could not read file, error {}", e); exit(1) }
+    };
+
+    let toks = tokenize(src, &mut dur);
     println!("{:?}", toks);
 }
